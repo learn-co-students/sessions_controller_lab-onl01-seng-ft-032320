0895ffe2e5523f01b543027f050793a2f6113d58
Diane Crame <dcrame@icloud.com>
2020-05-31 15:29:14 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+    def index
+    end
+
+    def new
+       
+    end
+
+    def create
+        if params[:name].nil? || params[:name].empty?
+            redirect_to login_path
+        else
+            session[:name] = params[:name]
+            redirect_to '/'
+        end
+    end
+
+    def destroy
+        session.delete :name
+    end
+end
